02f9aabb6601a3bb2d67941ff81e7703f0ecbbf8
Nahuel <n.peraza04@gmail.com>
2022-09-20 19:24:08 -0300

    responsive

--- frontend/assets/layout.js
+++ frontend/assets/layout.js
@@ -1,11 +1,18 @@
 const link = document.getElementById('css');
 const icoMoon = document.getElementById('icoMoon');
 const icoSun = document.getElementById('icoSun')
+const img = document.getElementById('imgLogo');
 
 
 console.log(link);
 console.log(icoMoon);
 console.log(icoSun);
 
-icoMoon.addEventListener('click', () => (link.setAttribute('href', "assets/loginNight.css")));
-icoSun.addEventListener('click', () => (link.setAttribute('href', "assets/login.css")))
+icoMoon.addEventListener('click', () => {
+    link.setAttribute('href', "assets/loginNight.css")
+    imgLogo.setAttribute('src', "assets/img/LogoFDWhite.png");
+});
+icoSun.addEventListener('click', () => {
+    link.setAttribute('href', "assets/login.css");
+    imgLogo.setAttribute('src', "assets/img/LogoFD.png")
+})
