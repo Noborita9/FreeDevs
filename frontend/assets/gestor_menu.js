534c8d3d3ffdfda6b5b392a96f7cfad2e1890b9d
Nahuel <n.peraza04@gmail.com>
2022-09-22 12:43:05 -0300

    gestor de productos

--- frontend/assets/gestor_menu.js
+++ frontend/assets/gestor_menu.js
@@ -1,12 +1,15 @@
 const menuInsumos = document.getElementById('menu_insumos');
 const menuEventos = document.getElementById('menu_eventos');
-const menuUsuarios = document.getElementById('menu_usuarios')
+const menuUsuarios = document.getElementById('menu_usuarios');
+const menuProductos = document.getElementById('menu_productos')
 const insumosBoton = document.getElementById('insumos');
 const eventosBoton = document.getElementById('eventos');
-const usuariosBoton = document.getElementById('usuarios')
+const usuariosBoton = document.getElementById('usuarios');
+const productosBoton = document.getElementById('productos');
 
 
 insumosBoton.addEventListener('click', () => {
+    menuProductos.style.display = "none";
     menuUsuarios.style.display = "none";
     menuEventos.style.display = "none";
     menuInsumos.style.display = "flex";
@@ -14,6 +17,7 @@ insumosBoton.addEventListener('click', () => {
 })
 
 eventosBoton.addEventListener('click', () => {
+    menuProductos.style.display = "none";
     menuUsuarios.style.display = "none";
     menuEventos.style.display = "flex";
     menuInsumos.style.display = "none";
@@ -21,7 +25,16 @@ eventosBoton.addEventListener('click', () => {
 })
 
 usuariosBoton.addEventListener('click', () => {
+    menuProductos.style.display = "none";
     menuUsuarios.style.display = "flex";
+    menuEventos.style.display = "none";
+    menuInsumos.style.display = "none";
+    console.log('usuarios eventos')
+})
+
+productosBoton.addEventListener('click', () => {
+    menuProductos.style.display = "flex";
+    menuUsuarios.style.display = "none";
     menuEventos.style.display = "none";
     menuInsumos.style.display = "none";
     console.log('usuarios eventos')
