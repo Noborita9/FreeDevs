1246b4181598ba1b330e9a0ba99edf3f1cea415a
Nahuel <n.peraza04@gmail.com>
2022-09-22 11:47:26 -0300

    diseño del market

--- frontend/assets/gestor_menu.js
+++ frontend/assets/gestor_menu.js
@@ -3,16 +3,14 @@ const menuEventos = document.getElementById('menu_eventos');
 const insumosBoton = document.getElementById('insumos');
 const eventosBoton = document.getElementById('eventos');
 
-insumosBoton.addEventListener('click', () => 
-{
-menuEventos.style.zIndex = "1";
-menuInsumos.style.zIndex = "2";
-console.log('arriba insumos')
+insumosBoton.addEventListener('click', () => {
+    menuEventos.style.display = "none";
+    menuInsumos.style.display = "flex";
+    console.log('arriba insumos')
 })
 
-eventosBoton.addEventListener('click', () => 
-{
-menuInsumos.style.zIndex = "1";
-menuEventos.style.zIndex = "2";
-console.log('arriba eventos')
+eventosBoton.addEventListener('click', () => {
+    menuEventos.style.display = "flex";
+    menuInsumos.style.display = "none";
+    console.log('arriba eventos')
 })
