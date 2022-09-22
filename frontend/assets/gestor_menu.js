2dc147bf8e0d529a2fcf418e761a5fde0d99a2cd
Nahuel <n.peraza04@gmail.com>
2022-09-22 12:26:57 -0300

    gestor de usuarios

--- frontend/assets/gestor_menu.js
+++ frontend/assets/gestor_menu.js
@@ -1,16 +1,28 @@
 const menuInsumos = document.getElementById('menu_insumos');
 const menuEventos = document.getElementById('menu_eventos');
+const menuUsuarios = document.getElementById('menu_usuarios')
 const insumosBoton = document.getElementById('insumos');
 const eventosBoton = document.getElementById('eventos');
+const usuariosBoton = document.getElementById('usuarios')
+
 
 insumosBoton.addEventListener('click', () => {
+    menuUsuarios.style.display = "none";
     menuEventos.style.display = "none";
     menuInsumos.style.display = "flex";
     console.log('arriba insumos')
 })
 
 eventosBoton.addEventListener('click', () => {
+    menuUsuarios.style.display = "none";
     menuEventos.style.display = "flex";
     menuInsumos.style.display = "none";
     console.log('arriba eventos')
+})
+
+usuariosBoton.addEventListener('click', () => {
+    menuUsuarios.style.display = "flex";
+    menuEventos.style.display = "none";
+    menuInsumos.style.display = "none";
+    console.log('usuarios eventos')
 })
