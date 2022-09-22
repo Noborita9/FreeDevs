e60c7f7691b81f6df00c1766e8c64971bec3d775
noborita9 <joaquinbonora9@gmail.com>
2022-09-22 12:45:58 -0300

    add create usuario

---
+++ backend/api/create_usuario.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_POST["usuario"]) && !empty($_POST["usuario"]) && isset($_POST["password"]) && !empty($_POST["password"]) && isset($_POST["role"]) && !empty($_POST["role"])) {check_user_exists();}else {
+  //header("Location: ../../frontend/index.html ");
+  exit();
+}
+function check_user_exists() {
+  include("../conexion.php");
+  $stmt = $pdo->prepare("SELECT nombre_usuario FROM usuarios WHERE nombre_usuario = ?;");
+  $stmt->execute($_POST["usuario"]);
+  $data = $stmt->fetchAll();
+  foreach ($data as $row) {
+    if (strcmp($_POST["usuario"], $row["nombre_usuario"] == 0)){
+      echo "User Already Exists";
+      exit();
+    }
+  }
+  create_user();
+}
+
+function create_user() {
+  include("../conexion.php");
+  $stmt = $pdo->prepare("INSERT INTO usuarios (nombre_usuario, contrasena, role) VALUES (:username, :password, :role)");
+  $stmt->bindParam(":username", $_POST["usuario"]);
+  $stmt->bindParam(":password", $_POST["password"]);
+  $stmt->bindParam(":role", $_POST["role"]);
+  $stmt->execute();
+}
+
+?>
