39e1350d44fdcf8f65330ddab575d3e99e5c0fde
Nahuel <n.peraza04@gmail.com>
2022-09-27 19:15:08 -0300

    limpieza de codigo + menues, plato, mobiliario y ficha tecnica

--- frontend/assets/gestor_menu.js
+++ frontend/assets/gestor_menu.js
@@ -1,18 +1,33 @@
+// menu administrativo
 const menuInsumos = document.getElementById('menu_insumos');
 const menuEventos = document.getElementById('menu_eventos');
 const menuUsuarios = document.getElementById('menu_usuarios');
 const menuProductos = document.getElementById('menu_productos');
-const menuRoles = document.getElementById('menu_roles')
+const menuRoles = document.getElementById('menu_roles');
+const menuMobiliarios = document.getElementById('menu_mobiliarios');
+const menuMenues = document.getElementById('menu_menues');
+const menuPlatos = document.getElementById('menu_platos');
+const menuFichTec = document.getElementById('menu_fichasTecnicas');
+
+// barra de navegacion
 const insumosBoton = document.getElementById('insumos');
 const eventosBoton = document.getElementById('eventos');
 const usuariosBoton = document.getElementById('usuarios');
 const productosBoton = document.getElementById('productos');
 const rolesBoton = document.getElementById('roles');
+const mobiliariosBoton = document.getElementById('mobiliarios');
+const menuesBoton = document.getElementById('menues');
+const platosBoton = document.getElementById('platos');
+const fichTecBoton = document.getElementById('fichasTecnicas');
 
 menuRoles.style.display = "none";
 menuProductos.style.display = "none";
 menuUsuarios.style.display = "none";
 menuEventos.style.display = "none";
+menuMobiliarios.style.display = "none";
+menuMenues.style.display = "none";
+menuPlatos.style.display = "none";
+menuFichTec.style.display = "none";
 menuInsumos.style.display = "flex";
 
 insumosBoton.addEventListener('click', () => {
@@ -21,7 +36,10 @@ insumosBoton.addEventListener('click', () => {
     menuUsuarios.style.display = "none";
     menuEventos.style.display = "none";
     menuInsumos.style.display = "flex";
-    console.log('arriba insumos')
+    menuMobiliarios.style.display = "none";
+    menuMenues.style.display = "none";
+    menuPlatos.style.display = "none";
+    menuFichTec.style.display = "none";
 })
 
 eventosBoton.addEventListener('click', () => {
@@ -30,7 +48,10 @@ eventosBoton.addEventListener('click', () => {
     menuUsuarios.style.display = "none";
     menuEventos.style.display = "flex";
     menuInsumos.style.display = "none";
-    console.log('arriba eventos')
+    menuMobiliarios.style.display = "none";
+    menuMenues.style.display = "none";
+    menuPlatos.style.display = "none";
+    menuFichTec.style.display = "none";
 })
 
 usuariosBoton.addEventListener('click', () => {
@@ -39,7 +60,10 @@ usuariosBoton.addEventListener('click', () => {
     menuUsuarios.style.display = "flex";
     menuEventos.style.display = "none";
     menuInsumos.style.display = "none";
-    console.log('usuarios eventos')
+    menuMobiliarios.style.display = "none";
+    menuMenues.style.display = "none";
+    menuPlatos.style.display = "none";
+    menuFichTec.style.display = "none";
 })
 
 productosBoton.addEventListener('click', () => {
@@ -48,7 +72,10 @@ productosBoton.addEventListener('click', () => {
     menuUsuarios.style.display = "none";
     menuEventos.style.display = "none";
     menuInsumos.style.display = "none";
-    console.log('usuarios eventos')
+    menuMobiliarios.style.display = "none";
+    menuMenues.style.display = "none";
+    menuPlatos.style.display = "none";
+    menuFichTec.style.display = "none";
 })
 
 rolesBoton.addEventListener('click', () => {
@@ -57,5 +84,56 @@ rolesBoton.addEventListener('click', () => {
     menuUsuarios.style.display = "none";
     menuEventos.style.display = "none";
     menuInsumos.style.display = "none";
-    console.log('usuarios eventos')
+    menuMobiliarios.style.display = "none";
+    menuMenues.style.display = "none";
+    menuPlatos.style.display = "none";
+    menuFichTec.style.display = "none";
+})
+
+mobiliariosBoton.addEventListener('click', () => {
+    menuRoles.style.display = "none";
+    menuProductos.style.display = "none";
+    menuUsuarios.style.display = "none";
+    menuEventos.style.display = "none";
+    menuInsumos.style.display = "none";
+    menuMobiliarios.style.display = "flex";
+    menuMenues.style.display = "none";
+    menuPlatos.style.display = "none";
+    menuFichTec.style.display = "none";
+})
+
+menuesBoton.addEventListener('click', () => {
+    menuRoles.style.display = "none";
+    menuProductos.style.display = "none";
+    menuUsuarios.style.display = "none";
+    menuEventos.style.display = "none";
+    menuInsumos.style.display = "none";
+    menuMobiliarios.style.display = "none";
+    menuMenues.style.display = "flex";
+    menuPlatos.style.display = "none";
+    menuFichTec.style.display = "none";
+})
+
+platosBoton.addEventListener('click', () => {
+    menuRoles.style.display = "none";
+    menuProductos.style.display = "none";
+    menuUsuarios.style.display = "none";
+    menuEventos.style.display = "none";
+    menuInsumos.style.display = "none";
+    menuMobiliarios.style.display = "none";
+    menuMenues.style.display = "none";
+    menuPlatos.style.display = "flex";
+    menuFichTec.style.display = "none";
+})
+
+fichTecBoton.addEventListener('click', () => {
+    menuRoles.style.display = "none";
+    menuProductos.style.display = "none";
+    menuUsuarios.style.display = "none";
+    menuEventos.style.display = "none";
+    menuInsumos.style.display = "none";
+    menuMobiliarios.style.display = "none";
+    menuMenues.style.display = "none";
+    menuPlatos.style.display = "none";
+    menuFichTec.style.display = "flex";
 })
