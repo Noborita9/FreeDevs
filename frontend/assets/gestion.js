db312d7d243a86ae8bea9dc4360f5d08b4be55a1
Noborita9 <joaquinbonora9@gmail.com>
2022-10-03 10:25:30 -0300

    loaders advanced

--- frontend/assets/gestion.js
+++ frontend/assets/gestion.js
@@ -17,17 +17,3 @@ function fetch_insumo(data){
   .catch((text)=> {console.log(text)})
 }
 
-function verify_fetch_insumo(){
-  const data = new FormData(document.getElementById("insumo_form"))
-  let nombre = data.get("nombre")
-  let stock = data.get("stock")
-  let unidad = data.get("unidad")
-  console.table(data)
-
-  if (nombre.length > 4 && unidad.length > 3){
-    fetch_insumo(data)
-  }
-  else {
-    console.log("Datos incorrectos")
-  }
-}
--- frontend/assets/loaders.js
+++ frontend/assets/loaders.js
@@ -1,3 +1,5 @@
+const backend_route = "../backend/api/"
+
 const loader = (url, element, injector, item_table) => {
   const data = new FormData()
   data.set("item", item_table)
@@ -25,42 +27,100 @@ const loader = (url, element, injector, item_table) => {
   // [{}]
 }
 
+function create_item(data, route){
+
+  fetch(`${backend_route}${route}`,{
+    method: "POST",
+    body: data
+  })
+  .then((res)=> {
+      if (res.ok) {
+        return res.text() } else {
+        throw "Error"
+      }
+    })
+  .then((text)=> {
+      console.log(text)
+    })
+  .catch((text)=> {console.log(text)})
+}
+
 const onClickLoadEventos = (data, element) => {
   if (element == "list_eventos") {
     return data.map((item) => {
     return `<span id='event_${item['id_evento']}'><h2>${item['titulo']}</h2><p>fecha xx/xx/xxxx</p></span>`
   })}
+}
+const onClickLoadInsumos = (data,element) => {
   if (element == "list_insumos") {
     return data.map((item) => {
     return `<span id='event_${item['id']}'><h2>${item['nombre']}</h2><span class="list_sub_data"><p>${item['stock']} </p> <p>${item['unidad']}</p></span></span>`
   })}
+}
+const onClickLoadProductos = (data,element) => {
   if (element == "list_productos") {
     return data.map((item) => {
     return `<p id='event_${item['id_evento']}'>${item['titulo']}</p>`
   })}
+}
+const onClickLoadUsuarios = (data,element) => {
   if (element == "list_usuarios") {
     return data.map((item) => {
     return `<p id='event_${item['id_evento']}'>${item['titulo']}</p>`
   })}
+}
+const onClickLoadRoles = (data,element) => {
   if (element == "list_roles") {
     return data.map((item) => {
     return `<span id='event_${item['id']}'><h2>${item['nombre']}</h2></span>`
   })}
 }
 
-document.getElementById("eventos").addEventListener("click", ()=>{
-  loader('../backend/api/load_items.php', "list_eventos", onClickLoadEventos, "eventos")
-})
-document.getElementById("insumos").addEventListener("click", ()=>{
-  loader('../backend/api/load_items.php', "list_insumos", onClickLoadEventos, "insumos")
-})
-document.getElementById("usuarios").addEventListener("click", ()=>{
-  loader('../backend/api/load_items.php', "list_usuarios", onClickLoadEventos, "usuarios")
-})
-document.getElementById("productos").addEventListener("click", ()=>{
-  loader('../backend/api/load_items.php', "list_productos", onClickLoadEventos, "productos")
-})
-document.getElementById("roles").addEventListener("click", ()=>{
-  loader('../backend/api/load_items.php', "list_roles", onClickLoadEventos, "role")
-})
+const options = [
+  {
+    "type": "eventos",
+    "loadFunction": onClickLoadEventos,
+    "submitHandlerFile": "create_evento.php",
+    "removeHandlerFile": "remove_evento.php"
+  },
+  {
+    "type": "roles",
+    "loadFunction": onClickLoadRoles,
+    "submitHandlerFile": "create_role.php",
+    "removeHandlerFile": "remove_role.php"
+  },
+  {
+    "type": "insumos",
+    "loadFunction": onClickLoadInsumos,
+    "submitHandlerFile": "create_insumo.php",
+    "removeHandlerFile": "remove_insumo.php"
+  },
+  {
+    "type": "productos",
+    "loadFunction": onClickLoadProductos,
+    "submitHandlerFile": "create_producto.php",
+    "removeHandlerFile": "remove_producto.php"
+  },
+  {
+    "type": "usuarios",
+    "loadFunction": onClickLoadUsuarios,
+    "submitHandlerFile": "create_usuario.php",
+    "removeHandlerFile": "remove_usuario.php"
+  },
+]
 
+options.forEach(option => {
+  let type = option["type"]
+  console.log(`list_${type}`)
+  document.getElementById(`${type}`).addEventListener('click', ()=>{
+    loader(`${backend_route}load_items.php`, `list_${type}`, option["loadFunction"], type)
+  })
+  document.querySelector(`#form_${type} span button:nth-child(1)`).addEventListener('click', ()=>{
+    create_item(new FormData(document.getElementById(`form_${type}`)), option["submitHandlerFile"])
+    loader(`${backend_route}load_items.php`, `list_${type}`, option["loadFunction"], type)
+  })
+  document.querySelector(`#form_${type} span button:nth-child(2)`).addEventListener('click', ()=>{
+    create_item(new FormData(document.getElementById(`form_${type}`)), option["submitHandlerFile"] )
+    loader(`${backend_route}load_items.php`, `list_${type}`, option["loadFunction"], type)
+  })
+});
