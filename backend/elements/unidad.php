6c9627eb98914da1d020f8c78fcf67d2ede298d6
Joaquin Bonora <nobora@pop-os.localdomain>
2022-11-15 16:19:03 -0300

    add first selects

--- backend/elements/unidad.php
+++ backend/elements/unidad.php
@@ -1,32 +1,54 @@
 <?php
-include("../conexion.php");
-class Unidad{
+class Unidad
+{
+    // The default unit will be milliliter or grams
     public $id;
     public $nombre;
 
-    public function from_array($data){
+    public function from_array($data)
+    {
         $this->id = $data["id"];
         $this->nombre = $data["nombre"];
     }
 
-    public function set_nombre($nombre){
+    public function set_nombre($nombre)
+    {
         $this->nombre = $nombre;
     }
 
-    public function get_nombre(){
+    public function get_nombre()
+    {
         return $this->nombre;
     }
 
-    public function get_id(){
+    public function get_id()
+    {
         return $this->id;
     }
 
-    public function set_id($id){
+    public function set_id($id)
+    {
         $this->id = $id;
     }
 
-    public function insert() {
-        $query = "INSERT INTO unidad (id, nombre) VALUES (:id, :nombre);";
+    public function select_by_id($id)
+    {
+        include("../conexion.php");
+        $query = "SELECT * FROM unidades WHERE id=:id;";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        $data = $stmt->fetchAll();
+        if (!(json_encode($data) === false)) {
+            $json = json_encode(["status" => 200, "body" => $data]);
+        } else {
+            $json = json_encode(["status" => 404, "error" => "There are no elements"]);
+        }
+        return $json;
+    }
 
+    public function change_unit()
+    {
+        // TODO
     }
 }
