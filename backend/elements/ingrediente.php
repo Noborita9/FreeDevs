ffec8f7bfb419b6fe0070ffc9389da8cdeee2e7b
noborita9 <joaquinbonora9@gmail.com>
2022-11-17 11:25:01 -0300

    more changes, unidades almost finish

--- backend/elements/ingrediente.php
+++ backend/elements/ingrediente.php
@@ -1,70 +1,29 @@
 <?php
-include './UnidadClass.php';
-class Ingrediente{
+class Ingrediente
+{
     public $database_table = "ingrediente";
     public $id;
     public $nombre;
-    public $unidad;
-    public $tacc = False;
-    public $vegano = False;
-    public $mani = False;
-    public $lacteo = False;
+    public $id_unidad;
 
-    public function from_named_array($data) {
-        if (!isset($data)) {
-            return false;
-        }
-        
+    public function from_named_array($data)
+    {
         $this->id = $data["id"];
         $this->nombre = $data["nombre"];
-        $this->unidad = $data["unidad"];
+        $this->id_unidad = $data["id_unidad"];
     }
 
-    function alter($boolean){
-        if ($boolean){
-            return false;
-        } else {
-            return true;
-        }
-    }
-
-    function return_as_array(){
+    function return_as_array()
+    {
         return array(
-            'nombre' => $this->nombre, 
-            'unidad' => $this->unidad, 
-            'tacc' => $this->tacc, 
-            'vegano' => $this->vegano, 
-            'mani' => $this->mani, 
-            'lacteo' => $this->lacteo, 
+            'nombre' => $this->nombre,
+            'id_unidad' => $this->id_unidad,
+            'id' => $this->id,
         );
     }
 
-    function change_condition($condition){
-        switch ($condition) {
-            case 'tacc':
-                $this->tacc = $this->alter($this->tacc);
-                break;
-
-            case 'vegano':
-                $this->vegano = $this->alter($this->vegano);
-                break;
-
-            case 'mani':
-                $this->mani = $this->alter($this->mani);
-                break;
-
-            case 'lacteo':
-                $this->lacteo = $this->alter($this->lacteo);
-                break;
-            default:
-                return false;
-                break;
-        }
-        return true;
-    }
-
-    function get_insert_query(){
-        $query = "INSERT INTO " . 
+    function insert()
+    {
+        $query = "INSERT INTO ingredientes (id, nombre, id_unidad, activo)";
     }
 }
-?>
--- backend/elements/unidad.php
+++ backend/elements/unidad.php
@@ -11,24 +11,17 @@ class Unidad
         $this->nombre = $data["nombre"];
     }
 
-    public function set_nombre($nombre)
+    public function select_all()
     {
-        $this->nombre = $nombre;
-    }
-
-    public function get_nombre()
-    {
-        return $this->nombre;
-    }
-
-    public function get_id()
-    {
-        return $this->id;
-    }
-
-    public function set_id($id)
-    {
-        $this->id = $id;
+        include("../conexion.php");
+        $query = "SELECT * FROM unidades;";
+        $data = $conn->query($query)->fetchAll();
+        if (!(json_encode($data) === false)) {
+            $json = json_encode(["status" => 200, "body" => $data]);
+        } else {
+            $json = json_encode(["status" => 404, "error" => "There are no elements"]);
+        }
+        echo $json;
     }
 
     public function select_by_id($id)
@@ -44,7 +37,36 @@ class Unidad
         } else {
             $json = json_encode(["status" => 404, "error" => "There are no elements"]);
         }
-        return $json;
+        echo $json;
+    }
+
+    public function delete_by_id($id)
+    {
+        include("../conexion.php");
+        $query = "DELETE FROM unidades where id=:id;";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $id);
+        $stmt->execute();
+        // Change to check if its ok
+        echo 1;
+    }
+
+    public function insert($nombre)
+    {
+        include("../conexion.php");
+        $query = "INSERT INTO unidades (nombre) VALUES (:nombre);";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":nombre", $nombre);
+        $stmt->execute();
+        // Change to check if its ok
+        echo 1;
+        /* $data = $stmt->fetchAll(); */
+        /* if (!(json_encode($data) === false)) { */
+        /*     $json = json_encode(["status" => 200, "body" => $data]); */
+        /* } else { */
+        /*     $json = json_encode(["status" => 404, "error" => "There are no elements"]); */
+        /* } */
+        /* echo $json; */
     }
 
     public function change_unit()
