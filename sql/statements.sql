95a4f48b450dc6a5d3a4172feae86a70a88c6d73
nobora <nobora@pop-os.localdomain>
2022-11-21 18:51:53 -0300

    advance sql

--- sql/statements.sql
+++ sql/statements.sql
@@ -5,11 +5,10 @@ USE gestornomia;
 CREATE TABLE user_roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(20) NOT NULL UNIQUE
-)
+);
 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    // Idealy this should be checked to not be replicated, We live in a simulation
     username VARCHAR(50) NOT NULL,
     passwd VARCHAR(50) NOT NULL,
     rol VARCHAR(20) NOT NULL,
@@ -17,21 +16,13 @@ CREATE TABLE usuarios (
     FOREIGN KEY (rol) REFERENCES user_roles(nombre)
 );
 
-CREATE TABLE unidades (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    active BOOL NOT NULL,
-    nombre VARCHAR(30)
-)
-
 CREATE TABLE ingredientes (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    id_unidad INT NOT NULL,
+    unidad VARCHAR(25),
     nombre VARCHAR(50) NOT NULL,
-    // Our Stock
     stock INT NOT NULL,
     precio INT NOT NULL,
-    active BOOL NOT NULL,
-    FOREIGN KEY(id_unidad) REFERENCES unidades(id)
+    active BOOL NOT NULL
 );
 
 CREATE TABLE fichas_tecnicas (
@@ -45,7 +36,7 @@ CREATE TABLE ingred_por_ficha (
     id_ficha INT NOT NULL,
     cantidad INT NOT NULL,
     PRIMARY KEY (id_ingrediente, id_ficha),
-    FOREIGN KEY (id_ingrediente) REFERENCES insumos(id),
+    FOREIGN KEY (id_ingrediente) REFERENCES ingredientes(id),
     FOREIGN KEY (id_ficha) REFERENCES fichas_tecnicas(id)
 );
 
@@ -68,26 +59,18 @@ CREATE TABLE eventos (
     servicio VARCHAR (200) NOT NULL,
     fecha DATE NOT NULL,
     ubicacion VARCHAR (200) NOT NULL,
-    link_ubicacion VARCHAR (200) ,
     cantidad_personas INT NOT NULL ,
     contacto VARCHAR (100) NOT NULL,
-    image_name VARCHAR (100)
+    image_name VARCHAR (100),
+    mobiliario VARCHAR (200)
 );
 
 CREATE TABLE encargados(
-  id_persona INT  ,
-  id_evento INT  ,
-  PRIMARY KEY(id_persona, id_evento),
-  FOREIGN KEY (id_persona) REFERENCES personas(id),
+  id_evento INT,
+  nombre VARCHAR(40),
+  PRIMARY KEY(nombre, id_evento),
   FOREIGN KEY (id_evento) REFERENCES eventos(id)
 );
-
-CREATE TABLE prod_order (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(100) NOT NULL,
-    active BOOL NOT NULL,
-    desc VARCHAR(300) NOT NULL
-)
 
 CREATE TABLE platos(
     id INT PRIMARY KEY AUTO_INCREMENT,
@@ -97,15 +80,26 @@ CREATE TABLE platos(
 CREATE TABLE productos_menues (
     id_evento INT NOT NULL,
     id_producto INT NOT NULL,
-    id_tipo_comida INT NOT NULL,
     PRIMARY KEY (id_evento,id_producto),
-    FOREIGN KEY(id_evento) REFERENCES ,
-    FOREIGN KEY(id_tipo_comida) REFERENCES menues_eventos(id_menu),
+    FOREIGN KEY(id_evento) REFERENCES eventos(id),
     FOREIGN KEY(id_producto) REFERENCES productos(id)
 );
 
+CREATE TABLE prod_order (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(100) NOT NULL,
+    active BOOL NOT NULL,
+    description VARCHAR(300) NOT NULL
+);
+
+CREATE TABLE producto_per_prod_order (
+    id_producto INT ,
+    id_prod_order INT,
+    nombre VARCHAR(100) NOT NULL,
+    PRIMARY KEY(id_producto, id_prod_order),
+    FOREIGN KEY (id_prod_order) REFERENCES prod_order(id),
+    FOREIGN KEY (id_producto) REFERENCES productos(id)
+);
 
 
-// Add base roles
 INSERT INTO user_roles (nombre) VALUES("ADMIN"), ("USER");
-INSERT INTO unidades (nombre, active) VALUES("KILOS", true), ("LITROS", true), ("GRAMOS", true), ("MILILITROS", true), ("UNIDAD", true);
