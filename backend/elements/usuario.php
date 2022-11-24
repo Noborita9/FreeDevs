855c1613da3deea2bbb91803ca6963d655193332
nobora <nobora@pop-os.localdomain>
2022-11-24 12:27:01 -0300

    add user functions

--- backend/elements/usuario.php
+++ backend/elements/usuario.php
@@ -1,39 +1,81 @@
 <?php
-class Usuario {
+class Usuario
+{
 
-  public $username;
-  public $password;
-  public $role;
+    public $username;
+    public $password;
+    public $role;
 
-  function constructor($username, $password, $role){
-    $this->username  = $username;
-    $this->password  = $password;
-    $this->role      = $role;
-  }
+    public function from_array($data)
+    {
+        try {
+            $this->username = $data["username"];
+            $this->password = $data["password"];
+            $this->role = $data["role"];
+        } catch (Throwable $th) {
+        }
+    }
 
-  function getUsername(){
-    return $this->username;
-  }
+    public function select_all()
+    {
+        include("../conexion.php");
+        $query = "SELECT * FROM usuarios;";
+        $data = $conn->query($query)->fetchAll();
+        if (!(json_encode($data) === false)) {
+            $json = json_encode(["status" => 200, "body" => $data]);
+        } else {
+            $json = json_encode(["status" => 404, "error" => "There are no elements"]);
+        }
+        echo $json;
+    }
 
-  function setUsername($username){
-    $this->username = $username;
-  }
+    public function select_by_id()
+    {
+        include("../conexion.php");
+        $query = "SELECT * FROM unidades WHERE id=:id;";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $this->id);
+        $stmt->execute();
+        $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        if (!(json_encode($data) === false)) {
+            $json = json_encode(["status" => 200, "body" => $data]);
+        } else {
+            $json = json_encode(["status" => 404, "error" => "There are no elements"]);
+        }
+        echo $json;
+    }
 
-  function getPassword(){
-    return $this->password;
-  }
+    public function delete_by_id()
+    {
+        include("../conexion.php");
+        $query = "DELETE FROM unidades where id=:id;";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":id", $this->id);
+        $stmt->execute();
+        // Change to check if its ok
+        echo 1;
+    }
 
-  function setPassword($password){
-    $this->password = $password;
-  }
+    public function insert()
+    {
+        include("../conexion.php");
+        $query = "INSERT INTO unidades (nombre) VALUES (:nombre);";
+        $stmt = $conn->prepare($query);
+        $stmt->bindParam(":nombre", $this->nombre);
+        $stmt->execute();
+        // Change to check if its ok
+        echo 1;
+        /* $data = $stmt->fetchAll(); */
+        /* if (!(json_encode($data) === false)) { */
+        /*     $json = json_encode(["status" => 200, "body" => $data]); */
+        /* } else { */
+        /*     $json = json_encode(["status" => 404, "error" => "There are no elements"]); */
+        /* } */
+        /* echo $json; */
+    }
 
-  function getRole(){
-    return $this->role;
-  }
-
-  function setRole($role){
-    $this->role = $role;
-  }
-
+    public function change_unit()
+    {
+        // TODO
+    }
 }
-?>
