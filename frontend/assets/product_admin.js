3a53ab13c86f190a5a0d698a7dde4d48c1fb2550
Joaquin Bonora <joaquinbonora9@gmail.com>
2022-11-30 00:37:25 -0300

    product admin

--- frontend/assets/product_admin.js
+++ frontend/assets/product_admin.js
@@ -43,7 +43,6 @@ counterIngredient.value = 1
 
 const loadUsedIngredients = (data) => {
     return data.map((item) => {
-        console.log(item)
         return `
 <tr>
 <td><i class="fa-solid fa-trash icon-variant"></i></td>
@@ -56,6 +55,7 @@ const loadUsedIngredients = (data) => {
 
 const add_ingr_row = (loaded = false) => {
     let actual_option = selectIngredient.selectedIndex
+    console.log(actual_ing)
     if (actual_ing.has(actual_option)) {
         return
     } else {
@@ -79,9 +79,7 @@ const load_ingr_row = (item) => {
         id_producto: prod_id,
         stock: item.cantidad
     }
-    actual_ing.set(actual_option,
-        load_ingrd
-    )
+    actual_ing.set(actual_option, load_ingrd)
     let load_string = selectIngredient[load_ingrd.id - 1].value
     ingredientList.innerHTML += `<tr><td><i class="fa-solid fa-trash icon-variant"></i></td>
 <td>${load_string}</td><td>${load_ingrd.stock}</td></tr>`
@@ -102,19 +100,9 @@ const add_var_row = (times, add_new = false) => {
     for (let j = actual_unities.length; j < variant_counter; j++) {
         if (j >= 1) {
             let stock, price, nombre;
-            if (j == 1) {
-                nombre = document.querySelector(`#variant_list > span > input[type=text]:nth-child(2)`).value
-                price = document.querySelector(`#variant_list > span > input[type=number]:nth-child(3)`).value
-                stock = document.querySelector(`#variant_list > span > input[type=number]:nth-child(4)`).value
-            } else {
-
-                if (document.querySelector(`#variant_list > span:nth-child(${j}) > input[type=text]:nth-child(2)`).value.length == 0) {
-                    return
-                }
-                nombre = document.querySelector(`#variant_list > span:nth-child(${j}) > input[type=text]:nth-child(2)`).value
-                price = document.querySelector(`#variant_list > span:nth-child(${j}) > input[type=number]:nth-child(3)`).value
-                stock = document.querySelector(`#variant_list > span:nth-child(${j}) > input[type=number]:nth-child(4)`).value
-            }
+            nombre = document.querySelector(`#var_${j} > input.sep_var_unidad`).value
+            price = document.querySelector(`#var_${j} > input.sep_var_precio`).value
+            stock = document.querySelector(`#var_${j} > input.sep_var_stock`).value
             let new_unit = {
                 id: j,
                 unidad: nombre,
@@ -122,28 +110,29 @@ const add_var_row = (times, add_new = false) => {
                 stock: stock
             }
             actual_unities.push(new_unit)
+        } else {
+
         }
     }
     for (let j = 0; j < times; j++) {
         let variantInput =
             `<span id='var_${variant_counter}' class="variant none_margin"><i class="fa-solid fa-trash icon-variant"></i>
-<input type="text" placeholder="unidad"><input type="number" placeholder="precio"><input type="number" placeholder="stock"></span>`
+<input class="sep_var_unidad" type="text" placeholder="unidad"><input class="sep_var_precio" type="number" placeholder="precio"><input class="sep_var_stock" type="number" placeholder="stock"></span>`
 
         variantList.innerHTML += variantInput
         variant_counter += 1
 
     }
     // Restore values
-    console.log(actual_unities)
     actual_unities.forEach((item, index) => {
         if (actual_unities.length < 2) {
             document.querySelector(`#variant_list > span > input[type=text]:nth-child(2)`).value = item.unidad
             document.querySelector(`#variant_list > span > input[type=number]:nth-child(3)`).value = item.precio
             document.querySelector(`#variant_list > span > input[type=number]:nth-child(4)`).value = item.stock
         } else {
-            document.querySelector(`#variant_list > span:nth-child(${item.id}) > input[type=text]:nth-child(2)`).value = item.unidad
-            document.querySelector(`#variant_list > span:nth-child(${item.id}) > input[type=number]:nth-child(3)`).value = item.precio
-            document.querySelector(`#variant_list > span:nth-child(${item.id}) > input[type=number]:nth-child(4)`).value = item.stock
+            document.querySelector(`#variant_list > span:nth-child(${index + 1}) > input[type=text]:nth-child(2)`).value = item.unidad
+            document.querySelector(`#variant_list > span:nth-child(${index + 1}) > input[type=number]:nth-child(3)`).value = item.precio
+            document.querySelector(`#variant_list > span:nth-child(${index + 1}) > input[type=number]:nth-child(4)`).value = item.stock
         }
     })
 }
@@ -163,20 +152,9 @@ const save_producto = () => {
     for (let j = actual_unities.length; j < variant_counter; j++) {
         if (j >= 1) {
             let stock, price, nombre;
-            if (j == 1) {
-
-                stock = document.querySelector(`#variant_list > span > input[type=number]:nth-child(4)`).value
-                price = document.querySelector(`#variant_list > span > input[type=number]:nth-child(3)`).value
-                nombre = document.querySelector(`#variant_list > span > input[type=text]:nth-child(2)`).value
-            } else {
-
-                if (document.querySelector(`#variant_list > span:nth-child(${j}) > input[type=text]:nth-child(2)`).value.length == 0) {
-                    return
-                }
-                nombre = document.querySelector(`#variant_list > span:nth-child(${j}) > input[type=text]:nth-child(2)`).value
-                price = document.querySelector(`#variant_list > span:nth-child(${j}) > input[type=number]:nth-child(3)`).value
-                stock = document.querySelector(`#variant_list > span:nth-child(${j}) > input[type=number]:nth-child(4)`).value
-            }
+            nombre = document.querySelector(`#var_${j} > input.sep_var_unidad`).value
+            price = document.querySelector(`#var_${j} > input.sep_var_precio`).value
+            stock = document.querySelector(`#var_${j} > input.sep_var_stock`).value
             let new_unit = {
                 id: j,
                 unidad: nombre,
@@ -186,14 +164,13 @@ const save_producto = () => {
             actual_unities.push(new_unit)
         }
     }
-    console.log(`Act unities ${actual_unities}`)
     data.set("id", prod_id)
     data.set("searching", true)
     data.set("unidades", JSON.stringify(actual_unities))
     data.set("ingredientes", JSON.stringify(ingreds))
     data.set("query", prod_id)
     data.set("query_attr", "id_producto")
-    fetch(`${backendRoute_prod}search_items.php`, {
+    fetch(`${backendRoute_prod}create_producto.php`, {
         method: "POST",
         body: data
     })
@@ -205,7 +182,6 @@ const save_producto = () => {
             }
         })
         .then((texto) => {
-            console.log(texto)
             productLoader(false, getProductosHtml)
         })
         .catch((err) => { console.log(err) })
@@ -214,7 +190,7 @@ const save_producto = () => {
 const loadUnities = (id) => {
     const data = new FormData()
     data.set("item", "product_per_unity")
-    data.set("query", `%${id}%`)
+    data.set("query", `${id}`)
     data.set("query_attr", "id_producto")
     fetch(`${backendRoute_prod}search_items.php`, {
         method: "POST",
@@ -234,7 +210,6 @@ const loadUnities = (id) => {
             variant_counter = 1
             let data = json["body"]
             actual_unities = data
-            console.log(actual_unities)
             add_var_row(data.length)
         })
         .catch((err) => { console.log(err) })
@@ -272,9 +247,11 @@ const chargeProduct = (id, clear) => {
     if (clear) {
         ingredientList.innerHTML = " "
         variantList.innerHTML = " "
+        variant_counter = 1
+        add_var_row(1, true)
         prod_id = 0
+        actual_unities = []
         nombre_in_form.value = ""
-        products.clear()
     } else {
         let act_item = products.get(id)
         nombre_in_form.value = act_item.nombre
@@ -282,6 +259,7 @@ const chargeProduct = (id, clear) => {
         loadUnities(id)
         loadIngredients(id)
     }
+    console.log(prod_id)
 }
 
 const getProductosHtml = (data) => {
